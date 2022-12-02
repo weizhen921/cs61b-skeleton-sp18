d2347b7bee162c73accf7b9e7e5687a8ff32c5df
weizhen <973590042@qq.com>
2022-12-02 12:25:16 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,18 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            sum = sum + x;
+            System.out.print(sum + " ");
+            x = x + 1;
+        }
+
+    }
+}
